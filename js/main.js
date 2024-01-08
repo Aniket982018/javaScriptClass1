1369a8f5a106a84b5851edb535e678364ddf43d3
Aniket-Puku <aniket@Aniket-Pukus-MacBook-Pro.local>
2024-01-08 16:51:11 -0500

    first Push

---
+++ js/main.js
@@ -0,0 +1,19 @@
+console.log("I am my own file");
+
+var num = 5;
+
+let str = "Hello World";
+
+const MAX = 14;
+
+num = 6;
+str = "MY WORLD"
+
+let numArray = [3, 5, 2, 6];
+console.log(numArray);
+
+console.log(numArray[3]);
+
+
+
+//number => natural and floating points numbers
